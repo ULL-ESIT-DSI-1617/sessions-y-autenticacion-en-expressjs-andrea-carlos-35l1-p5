bf500d599f0a482c98da382e97f0bc1023f5080f
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-03-19 17:06:11 +0000

    Añadido metodo post de login

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ app.get('/login', function(req, res){
   res.render('login');
 });
 
+//Obtiene la respuesta del formulario y comprueba si es correcto
+app.post('/login', function(req, res){
+    if (!req.body.username || !req.body.password) {
+      console.log('login failed');
+        res.render('noautentificado', { message: 'No deje ningún campo vacio' } );
+    } else if(req.body.username in users &&
+              bcrypt.compareSync(req.body.password, users[req.body.username])) {
+      req.session.user = req.body.username;
+      req.session.admin = true;
+      console.log('registrado');
+      res.render('autentificado', { message: 'Estas autentificado, accede al contenido' } );
+    } else {
+      console.log('login failed');
+      res.render('noautentificado', { message: 'Login Failed' } );
+    }
+  });
+
 app.use('/content', express.static(path.join(__dirname, 'public')));
 
   var server = app.listen(8087, function() {
