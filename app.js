b8e15e4a527c1acebd9f0541180e98b89e7ac44d
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-03-19 12:47:33 +0000

    Cambios readme

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 
 app.use('/content', express.static(path.join(__dirname, 'public')));
 
-  var server = app.listen(8080, function() {
+  var server = app.listen(8085, function() {
 	var host = server.address().address
 	var port = server.address().port
 
-	console.log('Conectado al puerto 8080')
+	console.log('Conectado al puerto 8085')
 })
