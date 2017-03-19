7db41b1502ef9eeee4bdf750eecdad224d4fba82
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-03-19 13:37:54 +0000

    Modificado puerto de escucha y añadido formulario de login close #3

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Muestra la vista con el formulario para log in
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
 app.use('/content', express.static(path.join(__dirname, 'public')));
 
-  var server = app.listen(8085, function() {
+  var server = app.listen(8087, function() {
 	var host = server.address().address
 	var port = server.address().port
 
-	console.log('Conectado al puerto 8085')
+	console.log('Conectado al puerto 8087')
 })
